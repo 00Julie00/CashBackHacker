bf893965d2e8099c04928dc02655a465f1300889
Julia Rusanova <elmarenelcorazon@gmail.com>
2023-03-28 02:33:27 +0300

    modified CashBackHackServiceTest.java

--- src/test/java/ru/netology/service/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -7,43 +7,34 @@ public class CashBackHackServiceTest {
 
     @Test
     public void shouldRemainderIfPurchase1000() {
-        int actual = service.remain(1000);
-        int expected = 0;
-        assertEquals(actual, expected);
+        int amount = 1000;
+        assertEquals(0, service.remain(amount));
     }
+
 
     @Test
     public void shouldRemainderAbout1000IfPurchase0() {
-        int actual = service.remain(0);
-        int expected = 1000;
-        assertEquals(actual, expected);
+        int amount = 0;
+        assertEquals(1000, service.remain(amount));
     }
+
 
     @Test
     public void shouldRemainderAbout500IfPurchase500() {
-        int actual = service.remain(500);
-        int expected = 500;
-        assertEquals(actual, expected);
+        int amount = 500;
+        assertEquals(500, service.remain(amount));
     }
 
     @Test
     public void shouldRemainderIfPurchase2500() {
-        int actual = service.remain(2500);
-        int expected = 500;
-        assertEquals(actual, expected);
+        int amount = 2500;
+        assertEquals(500, service.remain(amount));
     }
 
     @Test
     public void shouldRemainIfPurchase999() {
-        int actual = service.remain(999);
-        int expected = 1;
-        assertEquals(actual, expected);
-    }
-    @Test
-    public void shouldRemainIfPurchase1001() {
-        int actual = service.remain(1001);
-        int expected = 999;
-        assertEquals(actual, expected);
+        int amount = 999;
+        assertEquals(1, service.remain(amount));
     }
 
 }
