801f7cacf5980eb3c053dcbdfc084e1691dde34e
Julia Rusanova <elmarenelcorazon@gmail.com>
2023-03-28 02:19:19 +0300

    Add tests with JUnit Jupiter

--- src/test/java/ru/netology/service/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -1,6 +1,6 @@
 package ru.netology.service;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashBackHackServiceTest {
     CashBackHackService service = new CashBackHackService();
@@ -9,41 +9,41 @@ public class CashBackHackServiceTest {
     public void shouldRemainderIfPurchase1000() {
         int actual = service.remain(1000);
         int expected = 0;
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldRemainderAbout1000IfPurchase0() {
         int actual = service.remain(0);
         int expected = 1000;
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldRemainderAbout500IfPurchase500() {
         int actual = service.remain(500);
         int expected = 500;
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldRemainderIfPurchase2500() {
         int actual = service.remain(2500);
         int expected = 500;
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldRemainIfPurchase999() {
         int actual = service.remain(999);
         int expected = 1;
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
     @Test
     public void shouldRemainIfPurchase1001() {
         int actual = service.remain(1001);
         int expected = 999;
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
 }
